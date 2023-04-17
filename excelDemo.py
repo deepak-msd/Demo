36bceac45eba3c1d225274b0af4896469211bd0a
deepak-msd <msd0023@gmail.com>
2023-04-17 18:22:11 +0530

    2nd row changes

--- excelDemo.py
+++ excelDemo.py
@@ -12,7 +12,7 @@ print(sheet['A4'].value)
 for i in range(1, sheet.max_row+1):
     if sheet.cell(row=i, column=1).value == "Testcase2":
 
-        for j in range(1, sheet.max_column+1):
+        for j in range(2, sheet.max_column+1):
             Dict[sheet.cell(row=1, column=j).value]= sheet.cell(row=i, column=j).value
 
 print(Dict)
